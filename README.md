6dcce566b68eaf2e4e66ed1696199a2a16383702
Loo Guan Wee <guanwee_loo@hotmail.com>
2021-06-05 12:57:24 -0400

    Consume ATM APIs

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # airtechchallenge2021
+
+##To execute after performing "mvn package"
+ java -jar target/AIRTechChallenge-0.0.1-SNAPSHOT.jar --api.key=$ATM_API_KEY 
+
+Replace $ATM_API_KEY with your API Key
+
+##Retrieve all airports
+HTTP GET http://localhost:8080/airports 
+
+##Retrieve all SIDS in airport
+HTTP GET http://localhost:8080/sids/airport/{icao}
+
+##Retrieve all STARS in airport
+HTTP GET http://localhost:8080/stars/airport/{icao}
+
--- src/main/java/org/loogw/airtechchallenge/WebApplication.java
+++ src/main/java/org/loogw/airtechchallenge/WebApplication.java
@@ -3,8 +3,16 @@ package org.loogw.airtechchallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @SpringBootApplication
 @RestController
@@ -12,13 +20,46 @@ public class WebApplication {
    @Value("${api.key}")
    private String apiKey;
 
+   @Autowired
+   private RestTemplate restTemplate;
+   @Bean
+   public RestTemplate restTemplate() {
+     return new RestTemplate();
+   }
+
+
    public static void main(String[] args) {
       SpringApplication.run(WebApplication.class, args);
    }
 
    @RequestMapping(value="/")
-   public String apiKey() {
-	return apiKey;
+   public String home() {
+	return "List all airports /airports <br> List SIDS in airport /sids/airport/{icao} <br> List STARS in airport /stars/airport/{icao}";
+   }
+
+   @RequestMapping(value="/airports")
+   public String getAirports() {
+	HttpHeaders headers = new HttpHeaders();
+	headers.set("api-key",apiKey);
+        HttpEntity <String> entity = new HttpEntity<String>(headers);
+        return restTemplate.exchange("https://open-atms.airlab.aero/api/v1/airac/airports",HttpMethod.GET, entity, String.class).getBody();
+
+   }
+
+   @RequestMapping(value = "/sids/airport/{icao}")
+   public String getSIDsForAirport(@PathVariable("icao") String id) {
+      HttpHeaders headers = new HttpHeaders();
+      headers.set("api-key",apiKey);
+      HttpEntity<String> entity = new HttpEntity<String>(headers);
+      return restTemplate.exchange("https://open-atms.airlab.aero/api/v1/airac/sids/airport/"+id,HttpMethod.GET, entity, String.class).getBody();
+   }
+
+   @RequestMapping(value = "/stars/airport/{icao}")
+   public String getSTARSForAirport(@PathVariable("icao") String id) {
+      HttpHeaders headers = new HttpHeaders();
+      headers.set("api-key",apiKey);
+      HttpEntity<String> entity = new HttpEntity<String>(headers);
+      return restTemplate.exchange("https://open-atms.airlab.aero/api/v1/airac/stars/airport/"+id,HttpMethod.GET, entity, String.class).getBody();
    }
 
 }
