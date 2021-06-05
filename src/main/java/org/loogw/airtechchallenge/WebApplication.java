e1e844fa00f99cfeeeeadb0ba5fa3098f75c350e
Loo Guan Wee <guanwee_loo@hotmail.com>
2021-06-05 11:37:06 -0400

    Some skeleton code for web service

--- src/main/java/org/loogw/airtechchallenge/WebApplication.java
+++ src/main/java/org/loogw/airtechchallenge/WebApplication.java
@@ -2,10 +2,23 @@ package org.loogw.airtechchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebApplication {
+   @Value("${api.key}")
+   private String apiKey;
+
    public static void main(String[] args) {
       SpringApplication.run(WebApplication.class, args);
    }
+
+   @RequestMapping(value="/")
+   public String apiKey() {
+	return apiKey;
+   }
+
 }
