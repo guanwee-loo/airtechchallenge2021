56cb88104c35a76ab1c28f0a7711e687a20b3b87
Loo Guan Wee <guanwee_loo@hotmail.com>
2021-06-13 07:51:05 -0400

    Add UI for fetch airports

--- frontend/src/Airport.test.js
+++ frontend/src/Airport.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Airport from './Airport';
 
-test('renders learn react link', () => {
-  render(<App />);
+test('renders airport link', () => {
+  render(<Airport />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+import ScotchInfoBar from './AIRInfoBar';
+import './styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Airport() {
+  const [airport, setAirports] = useState(null);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const fetchData = async () => {
+    const response = await axios.get(
+      'http://localhost:3000/api/airports'
+    );
+
+    setAirports(response.data);
+  };
+
+  return (
+    <div className="App">
+      <h2>Fetch Airports</h2>
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button" onClick={fetchData}>
+          Fetch Data
+        </button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+      <div className="airports">
+        {airport &&
+          airport.map((airport, index) => {
+
+            return (
+              <div className="airport" key={index}>
+                <h3>Airport {index + 1}</h3>
+                <h2>{airport.name}</h2>
+
+                <div className="details">
+                  <p>UID  : {airport.uid}</p>
+                  <p>ICAO : {airport.icao}</p>
+                  <p>LAT  : {airport.lat}</p>
+		  <p>LNG: {airport.lng}</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+
+      <ScotchInfoBar year="2021" />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Airport />, rootElement);
+
